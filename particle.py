932bda2c44addb0395bdac9ab3341782dbf2df43
kmounish <73723664+kmounish@users.noreply.github.com>
2023-08-31 10:18:11 -0400

    Add files via upload

---
+++ particle.py
@@ -0,0 +1,37 @@
+class Particle:
+    def __init__(self, x, y, size, id):
+        self.id = id
+        self.velocity = 0
+        self.gravity = 9.8
+        self.x = x
+        self.y = y
+        self.size = size
+
+    def get_pos(self):
+        return (self.x, self.y)
+
+    def update_pos(self, *args):
+        if len(args) == 0:
+            if self.y > 720:
+                self.y = 720
+            elif self.y == 720:
+                self.y = 720
+            else:
+                self.y = self.y + self.velocity
+        else:
+            self.y = args[0][1]-args[1]
+            if len(args) == 3:
+                if args[2] == "left":
+                    self.x = args[0][0]-args[1]
+                if args[2] == "right":
+                    self.x = args[0][0]+args[1]
+            self.update_velocity()
+
+    def update_velocity(self, *args):
+        if len(args) == 0:
+            self.velocity = 0
+        else:
+            self.velocity = (self.gravity*args[0])*.01 + self.velocity
+
+    def get_speed(self):
+        return self.velocity
---
+++ particle_sim.py
@@ -0,0 +1,83 @@
+import pygame
+import sys
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+from particle import Particle
+
+# TODO:
+# Maybe more: where it slides of to the side like sand or water
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+parts = []
+time = 0
+id = 0
+while running:
+    # Set up:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            print(pos)
+            tmp = Particle(pos[0], pos[1], size=5, id=id)
+            id += 1
+            parts.append(tmp)
+    screen.fill("white")
+    current_time = pygame.time.get_ticks()
+
+    # falling simulation:
+    for x in parts:
+        # test = True
+        right = True
+        left = True
+        down = True
+        x.update_velocity(current_time-time)
+        for y in parts:
+            check_left = (x.get_pos()[0]-y.size,
+                          x.get_pos()[1]-y.size) == y.get_pos()
+            check_right = (x.get_pos()[0]+y.size,
+                           x.get_pos()[1]-y.size) == y.get_pos()
+            check_down = (x.get_pos()[0],
+                          x.get_pos()[1]+y.size) == y.get_pos()
+
+            # test_1 = x.get_pos() == y.get_pos() and x.id is not y.id
+            # test_2 = (x.get_pos()[0], x.get_pos()[1]+y.size) == y.get_pos()
+            # test_3 = (x.get_pos()[0]+y.size,
+            #           x.get_pos()[1]+y.size) != y.get_pos()
+
+            if check_left:
+                left = False
+            if check_right:
+                right = False
+            if check_down:
+                down = False
+            # if x.get_pos() == y.get_pos() and x.id is not y.id:
+            #     test = False
+            #     x.update_pos(x.get_pos(), y.size)
+            #     break
+            # elif test_2:
+            #     test = False
+            #     break
+            # elif test_3:
+            #     test = False
+            #     x.update_pos(x.get_pos(), y.size, "left")
+        # if test:
+        #     x.update_pos()
+        if check_down and check_left and check_right:
+            x.update_pos()
+        elif not check_down and check_left and not check_right:
+            x.update_pos(x.get_pos(), x.size, "left")
+        elif not check_down and not check_left and check_right:
+            x.update_pos(x.get_pos(), x.size, "right")
+
+        pygame.draw.circle(screen, "green", x.get_pos(), x.size)
+
+    time = pygame.time.get_ticks()
+    pygame.display.flip()
+    clock.tick(30)
+
+pygame.quit()
